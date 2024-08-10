10082d1a8a5fcaf38004f8c697e61c77cddd1740
Vandielson <vandielson.tenorio.13@hotmail.com>
2024-08-10 17:54:24 -0300

    Criação do projeto

---
+++ gerenciamentofarmacia/src/main/java/br/com/farmacia/GerenciamentofarmaciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentofarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentofarmaciaApplication.class, args);
+	}
+
+}
