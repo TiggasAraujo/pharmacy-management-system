ad4836a75b687cc20141eff033e59925659503b7
Thiago2704 <thiagoaraujo909090@gmail.com>
2024-09-10 14:30:17 -0300

    alteração na classe VendaControllerTest

--- gerenciamentofarmacia/src/test/java/br/com/farmacia/controller/VendaControllerTest.java
+++ gerenciamentofarmacia/src/test/java/br/com/farmacia/controller/VendaControllerTest.java
@@ -19,6 +19,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -68,6 +69,7 @@ public class VendaControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "user", roles = {"USER"})
     public void testMostrarFormularioVenda() throws Exception {
         // Dados mockados para os serviços
         when(clienteService.buscarTodosClientes()).thenReturn(Arrays.asList(new Cliente(), new Cliente()));
